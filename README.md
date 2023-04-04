ab4cfac6790721906fa0aea4bc55cf5129a9f540
nico17022001 <nico17022001boolean@gmail.com>
2023-04-04 15:09:11 +0200

    primo-snack-finito

---
+++ README.md
@@ -0,0 +1,12 @@
+js-jsnacks-blocco-5
+===
+jSnack 1
+Abbiamo un elenco utenti (nome, cognome, età).
+Trovare i minorenni e gli over 65
+
+jSnack 2
+Abbiamo un elenco degli studenti di una facoltà
+Ogni studente ha un nome, un cognome, un numero di matricola e un elenco di voti.
+Dobbiamo creare un nuovo elenco dove ogni studente ha un nome-cognome, matricola e media voti
+*** BONUS
+Aggiungere la foto o l’avatar dello studente e stampare tutti gli studenti con delle card HTML in ordine alfabetico per cognome
---
+++ jSnack 1/script.js
@@ -0,0 +1,56 @@
+/*
+PASSAGGI 
+1-Creo un array con tutti gli utenti 
+2-Creo una costante per filtrare gli utenti minorenni
+3-Creo una costante per filtrare gli utenti maggiorenni
+4-creo un console.log(minorenni)
+4-creo un console.log(maggiorenni)
+
+*/
+
+
+
+
+
+
+
+
+let elencoUtenti = [
+  {
+    nome : "Mariolino",
+    cognome : "Mariolini",
+    eta : 70,
+  },
+  {
+    nome : "Paolo",
+    cognome : "Paolini",
+    eta : 17,
+  },
+  {
+    nome : "Franco",
+    cognome : "Franchini",
+    eta : 70,
+  },
+  {
+    nome : "Pierpaolo",
+    cognome : "Lavanda",
+    eta : 15,
+  },
+  {
+    nome : "Giuseppe",
+    cognome : "Ottomano",
+    eta : 68,
+  },
+  {
+    nome : "Asia",
+    cognome : "Gialli",
+    eta : 66,
+  },
+]
+
+const minorenni = elencoUtenti.filter(utente => utente.eta < 18);
+const maggiorenni = elencoUtenti.filter(utente => utente.eta > 65);
+
+console.log(maggiorenni)
+console.log(minorenni)
+
