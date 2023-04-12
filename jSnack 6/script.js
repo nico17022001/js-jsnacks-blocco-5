49757d1c580240ec4451a8a36b0883e061de7a23
nico17022001 <nico17022001boolean@gmail.com>
2023-04-12 19:32:49 +0200

    fine-esercizio-6

--- jSnack 6/script.js
+++ jSnack 6/script.js
@@ -1,4 +1,4 @@
-const squadra = [
+const squadre = [
   {
     nome : "Gialli",
     gol : 0,
@@ -28,5 +28,27 @@ const squadra = [
     gol : 0,
     subiti : 0,
   },
-
 ]
+
+function random() {
+  return  Math.floor(Math.random() * 10);
+}
+
+const squadraNew = squadre.map((squadra) => {
+  squadra.gol = random()
+  squadra.subiti = random()
+  return squadra
+})
+
+console.log(squadraNew)
+
+const squadre2 = squadre.map((squadra) =>{
+  const {nome,subiti} = squadra
+  const prova = {nome, subiti}
+  return prova
+})
+
+
+console.log(squadre2)
+
+
